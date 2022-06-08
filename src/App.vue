9e4fa4c98adfdff50cf029467a448b9a1ab14924
kwsk01 <kawasakitaiki0308@gmail.com>
2022-06-08 18:30:14 +0900

    commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Vue.js "/>
+  <Dotpng></Dotpng>
+  <Dotred></Dotred>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Dotpng from './components/Dotpng.vue'
+import Dotred from './components/Dotred.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Dotpng,
+    Dotred
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
