bfc3c2ae60dd68c8f1ffd77c69b6b3f710d8f5cb
kwsk01 <kawasakitaiki0308@gmail.com>
2022-06-15 00:47:41 +0900

    make pixel_art

---
+++ src/components/NewPixel.vue
@@ -0,0 +1,36 @@
+<template>
+    <canvas id="p1" width="200" height="200"></canvas>
+    <p>{{value}}</p>
+    
+</template>
+
+<script>
+    export default {
+
+        props: {
+            value: Array,
+        },
+       
+        components: {
+          
+        },
+
+        data() {
+            
+        },
+    
+        methods: {
+                window:onload = function() {  
+                const canvas2 = document.getElementById('p1');
+                const ctx2 = canvas2.getContext('2d');
+                ctx2.fillStyle = 'red';
+                ctx2.fillRect(0, 0, 200, 200);
+                console.log('red')
+            }
+        }
+    };
+
+</script>
+
+<style>
+</style>
