dc20b3a80465bcadfa7fefc5698ca1fe2f676d39
taikicoco <kawasakitaiki0308@gmail.com>
2022-09-05 03:19:01 +0900

    fix/cross_action

--- src/components/CrossIcon.vue
+++ src/components/CrossIcon.vue
@@ -1,26 +1,36 @@
 <template>
 <div class="border-y-4 border-black-600/100">
-    <div class="flex flex-wrap justify-around m-8">
+    <div class="absolute right-0 m-2">
+      <button class="rounded-lg"
+        v-on:click="close()"
+      ><img class="h-8 w-8" src="../assets/cross.png" alt="setting"></button>
+    </div>
+    <div class="flex flex-wrap items-center justify-around m-8">
         <canvas id="101" 
             :width = 'setting_icon.canvas_length' 
             :height = 'setting_icon.canvas_length' 
         ></canvas>
+        <div class="">
+        <img class="h-10 w-10" src="../assets/add.png" alt="cross">
+        </div>
         <canvas id="102" 
             :width = 'setting_icon.canvas_length' 
             :height = 'setting_icon.canvas_length' 
         ></canvas>
+        <img class="h-10 w-10" src="../assets/forward.png" alt="cross">
         <canvas id="103" 
             :width = 'setting_icon.canvas_length' 
             :height = 'setting_icon.canvas_length' 
         ></canvas>
         
     </div>
-
+    
     <button 
         v-on:click="create_cross_icon()" 
         class="button-30 cross_action_btn" 
         role="button"
     >合体させるアイコンを2つ選ぶ</button>
+
 
 </div>
 </template>
@@ -68,18 +78,14 @@ import {createIcon} from '../create_icon'
                 }
                 createIcon(id, this.setting_icon,icon_data)
             },
+
+            close:function() {
+                this.$emit('close','cross')
+            },
         }
     };
 
 </script>
 
 <style>
-    .cross_canvas { 
-        margin: 20px;
-    }
-
-    .cross_action_btn {
-        margin: 10px;
-        width: 300px;
-    }
 </style>
