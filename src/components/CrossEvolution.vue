531778f49bb234d5105be5eaea6dbf8409eab5fa
taikicoco <kawasakitaiki0308@gmail.com>
2022-09-07 00:49:31 +0900

    responsive

--- src/components/CrossEvolution.vue
+++ src/components/CrossEvolution.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="border-y-4 border-black-600/100">
-        <div class="absolute right-0 m-2">
+        
+        <div class="absolute right-0 m-2 ">
             <button class="rounded-lg"
                 v-on:click="close()"
             ><img class="h-8 w-8" src="../assets/cross.png" alt="setting"></button>
@@ -9,19 +10,20 @@
         <div class="flex justify-center">
             <div class="justify-around m-8">
                 <div class="flex flex-wrap items-center">
-                <canvas id="201" 
+                <canvas id="301" 
                     :width = 'setting_icon.canvas_length' 
                     :height = 'setting_icon.canvas_length' 
                 ></canvas>
                 <img class="h-10 w-10" src="../assets/forward.png" alt="cross">
-                <canvas id="202" 
+                <canvas id="302" 
                     :width = 'setting_icon.canvas_length' 
                     :height = 'setting_icon.canvas_length' 
                 ></canvas>
                 </div>
+
                 <div class="flex justify-center">
                     <button class="m-1 p-3 w-28 rounded-lg text-xs "
-                    v-on:click='create_evolution_icon("evolution")'
+                    v-on:click='select_action("evolution")'
                     >シンカ</button>
                 </div>        
             </div>
@@ -35,7 +37,7 @@ import {createIcon} from '../create_icon'
     export default {
 
         props: {
-            evolution_icon_data:Array,
+            cross_evolution_data:Array,
             setting_icon:Object,
         },
         
@@ -44,26 +46,27 @@ import {createIcon} from '../create_icon'
 
         data() {
             return{
-                icon_datas:this.evolution_icon_data,
+                icon_datas:this.cross_evolution_data,
             }
         },
     
         methods: {
 
-            create_evolution_icon:function() {
-                const size = this.setting_icon.pixel_number;
-                const point = Math.floor(Math.random()*(size*size/2));
-                const icon = this.evolution_icon_data[0]
-                icon[point] = (icon[point] + 1) % 2
-                createIcon(202,this.setting_icon,icon)  
+            create_cross_evolution:function() {
+                
             },
 
-            select_evolution_icon(icon_data) {
-                createIcon(201, this.setting_icon,icon_data)
+            select_cross_evolution(id,icon_data) {
+                if(id %2 == 0){
+                    id = 301;
+                }else {
+                    id = 302;
+                }
+                createIcon(id, this.setting_icon,icon_data)
             },
 
             close:function() {
-                this.$emit('close','evolution')
+                this.$emit('close','cross_evolution')
             },
         }
     };
--- src/components/CrossIcon.vue
+++ src/components/CrossIcon.vue
@@ -1,37 +1,38 @@
 <template>
 <div class="border-y-4 border-black-600/100">
+
     <div class="absolute right-0 m-2">
       <button class="rounded-lg"
         v-on:click="close()"
       ><img class="h-8 w-8" src="../assets/cross.png" alt="setting"></button>
     </div>
-    <div class="flex flex-wrap items-center justify-around m-8">
-        <canvas id="101" 
-            :width = 'setting_icon.canvas_length' 
-            :height = 'setting_icon.canvas_length' 
-        ></canvas>
-        <div class="">
-        <img class="h-10 w-10" src="../assets/add.png" alt="cross">
+
+    <div class="flex justify-center">
+        <div class="justify-around m-8">
+            <div class="flex flex-wrap items-center">
+            <canvas id="101" 
+                :width = 'setting_icon.canvas_length' 
+                :height = 'setting_icon.canvas_length' 
+            ></canvas>
+            <img class="h-10 w-10" src="../assets/add.png" alt="cross">
+            <canvas id="102" 
+                :width = 'setting_icon.canvas_length' 
+                :height = 'setting_icon.canvas_length' 
+            ></canvas>
+            <img class="h-10 w-10" src="../assets/forward.png" alt="cross">
+            <canvas id="103" 
+                :width = 'setting_icon.canvas_length' 
+                :height = 'setting_icon.canvas_length' 
+            ></canvas>
+            </div>
+
+            <div class="flex justify-center">
+                <button class="m-1 p-3 w-28 rounded-lg text-xs "
+                v-on:click="create_cross_icon()" 
+                >ガッタイ</button>
+            </div>        
         </div>
-        <canvas id="102" 
-            :width = 'setting_icon.canvas_length' 
-            :height = 'setting_icon.canvas_length' 
-        ></canvas>
-        <img class="h-10 w-10" src="../assets/forward.png" alt="cross">
-        <canvas id="103" 
-            :width = 'setting_icon.canvas_length' 
-            :height = 'setting_icon.canvas_length' 
-        ></canvas>
-        
     </div>
-    
-    <button 
-        v-on:click="create_cross_icon()" 
-        class="button-30 cross_action_btn" 
-        role="button"
-    >合体させるアイコンを2つ選ぶ</button>
-
-
 </div>
 </template>
 
